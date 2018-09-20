b141db3e2a01f44a8a47ecbf89b01b45d8fca5df
Sibaprasad Maiti <sibaprasad@Users-MacBook-Pro.local>
2018-09-20 17:55:24 +0530

    check added for test number less than 50

--- index.js
+++ index.js
@@ -15,9 +15,15 @@ const TEST_NUMBER = process.env.TEST_NUMBER || 7;
 // start tracking execution time
 console.time("execution time");
 
-// check if the test number is prime
-const isPrime = aks.checkIfPrime(TEST_NUMBER);
-console.log(`${TEST_NUMBER} is prime: ${isPrime}`);
+if (TEST_NUMBER < 51) {
+  // check if the test number is prime
+  const isPrime = aks.checkIfPrime(TEST_NUMBER);
+  console.log(`${TEST_NUMBER} is prime: ${isPrime}`);
+} else {
+  console.log(
+    "Please test with numbers less than 50. Otherwise the coefficients in the polynomial expansion get too big for node js to handle."
+  );
+}
 
 // end tracking execution time
 console.timeEnd("execution time");
