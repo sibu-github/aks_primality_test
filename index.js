f43c5dce8c311ec64e5f9e165453e5173a7c103e
Sibaprasad Maiti <sibaprasad@Users-MacBook-Pro.local>
2018-09-20 11:26:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * Main entry file
+ *
+ *
+ */
+
+// dependencies
+const aks = require("./lib/aks_test");
+
+// test number to test this module
+// defaulted to 7 if not provided in command line
+const TEST_NUMBER = process.env.TEST_NUMBER || 7;
+
+// start tracking execution time
+console.time("execution time");
+
+// check if the test number is prime
+const isPrime = aks.checkIfPrime(TEST_NUMBER);
+console.log(`${TEST_NUMBER} is prime: ${isPrime}`);
+
+// end tracking execution time
+console.timeEnd("execution time");
